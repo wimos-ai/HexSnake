deeb9467ffa00ae87673021ab9824f51acb22edd
Jacob Mosier <84042085+wimos-ai@users.noreply.github.com>
2026-07-06 18:34:37 -0500

    Initial commit

---
+++ src/raylib_game.c
@@ -0,0 +1,154 @@
+/*******************************************************************************************
+*
+*   raylib gamejam template
+*
+*   Code licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+*   BSD-like license that allows static linking with closed source software
+*
+*   Copyright (c) 2022-2026 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+#include "raylib.h"
+
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>      // Emscripten library
+#endif
+
+#include <stdio.h>                          // Required for: printf()
+#include <stdlib.h>                         // Required for: 
+#include <string.h>                         // Required for:
+
+//----------------------------------------------------------------------------------
+// Defines and Macros
+//----------------------------------------------------------------------------------
+// Simple log system to avoid printf() calls if required
+// NOTE: Avoiding those calls, also avoids const strings memory usage
+#define SUPPORT_LOG_INFO
+#if defined(SUPPORT_LOG_INFO)
+    #define LOG(...) printf(__VA_ARGS__)
+#else
+    #define LOG(...)
+#endif
+
+//----------------------------------------------------------------------------------
+// Types and Structures Definition
+//----------------------------------------------------------------------------------
+typedef enum { 
+    SCREEN_LOGO = 0, 
+    SCREEN_TITLE, 
+    SCREEN_GAMEPLAY, 
+    SCREEN_ENDING
+} GameScreen;
+
+// TODO: Define your custom data types here
+
+//----------------------------------------------------------------------------------
+// Global Variables Definition (local to this module)
+//----------------------------------------------------------------------------------
+static const int screenWidth = 720;
+static const int screenHeight = 720;
+
+static RenderTexture2D target = { 0 };  // Render texture to render our game
+static int frameCounter = 0;
+
+// TODO: Define global variables here, recommended to make them static
+
+//----------------------------------------------------------------------------------
+// Module Functions Declaration
+//----------------------------------------------------------------------------------
+static void UpdateDrawFrame(void);      // Update and Draw one frame
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
+{
+#if !defined(_DEBUG)
+    SetTraceLogLevel(LOG_NONE);         // Disable raylib trace log messages
+#endif
+
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    InitWindow(screenWidth, screenHeight, "raylib gamejam template");
+    
+    // TODO: Load resources / Initialize variables at this point
+    
+    // Render texture to draw, enables screen scaling
+    // NOTE: If screen is scaled, mouse input should be scaled proportionally
+    target = LoadRenderTexture(screenWidth, screenHeight);
+    SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR);
+
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+#else
+    SetTargetFPS(60);     // Set our game frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button
+    {
+        UpdateDrawFrame();
+    }
+#endif
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    UnloadRenderTexture(target);
+    
+    // TODO: Unload all loaded resources at this point
+
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
+
+//--------------------------------------------------------------------------------------------
+// Module Functions Definition
+//--------------------------------------------------------------------------------------------
+// Update and draw frame
+void UpdateDrawFrame(void)
+{
+    // Update
+    //----------------------------------------------------------------------------------
+    // TODO: Update variables / Implement example logic at this point
+   
+    frameCounter++;
+    //----------------------------------------------------------------------------------
+
+    // Draw
+    //----------------------------------------------------------------------------------
+    // Render game screen to a texture, 
+    // it could be useful for scaling or further shader postprocessing
+    BeginTextureMode(target);
+        ClearBackground(RAYWHITE);
+        
+        // TODO: Draw your game screen here
+
+        DrawRectangle(70, 90, 200, 200, BLACK);
+        DrawRectangle(70 + 16, 90 + 16, 200 - 32, 200 - 32, RAYWHITE);
+        DrawText("raylib", 70 + 200 - MeasureText("raylib", 40) - 32, 90 + 200 - 40 - 24, 40, BLACK);
+
+        DrawText("6.x", 290, 90 - 26, 280, BLACK);
+        DrawText("GAMEJAM", 70, 90 + 210, 120, MAROON);
+
+        if ((frameCounter/20)%2) DrawText("are you ready?", 160, 500, 50, BLACK);
+        
+        DrawRectangleLinesEx((Rectangle){ 0, 0, screenWidth, screenHeight }, 16, BLACK);
+        
+    EndTextureMode();
+    
+    // Render to screen (main framebuffer)
+    BeginDrawing();
+        ClearBackground(RAYWHITE);
+        
+        // Draw render texture to screen, scaled if required
+        DrawTexturePro(target.texture, (Rectangle){ 0, 0, (float)target.texture.width, -(float)target.texture.height }, 
+            (Rectangle){ 0, 0, (float)target.texture.width, (float)target.texture.height }, (Vector2){ 0, 0 }, 0.0f, WHITE);
+
+        // TODO: Draw everything that requires to be drawn at this point, maybe UI?
+
+    EndDrawing();
+    //----------------------------------------------------------------------------------  
+}
